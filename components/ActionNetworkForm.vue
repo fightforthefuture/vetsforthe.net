7b041ad71cd859b80f014f345ae258f8faeb9bb1
daytonyoung <36241804+daytonyoung@users.noreply.github.com>
2018-04-12 11:56:26 -0400

    Update ActionNetworkForm.vue

--- components/ActionNetworkForm.vue
+++ components/ActionNetworkForm.vue
@@ -6,7 +6,7 @@
     </div>
     <div class="row">
       <input v-model="company" type="text" placeholder="Company Name*" required="">
-      <input v-model="companyURL" type="text" placeholder="Company Website* (.com, LinkedIn, etc.)" required>
+      <input v-model="companyURL" type="text" placeholder="Company Website or LinkedIn*" required>
     </div>
     <div class="row">
       <input v-model="phone" type="tel" placeholder="Phone">
